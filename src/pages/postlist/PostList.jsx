939fd20906213ac295dfc9a5ad35cf91a49b8993
Eun-Ng <dhkdldms@gmail.com>
2022-08-08 14:29:29 +0900

    [남궁은] 1차 머지

--- src/pages/postlist/PostList.jsx
+++ src/pages/postlist/PostList.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import './PostList.scss';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import MovieCard from './MovieCard';
+
+const MOVIE_INFO = 'http://www.omdbapi.com/?apikey=8cd7b576&y=2022(';
 
 const PostList = () => {
-  return <div>PostList dev 브랜치 확인용입니다.</div>;
+  const fetchDataInfo = async () => {
+    const response = await axios.get(MOVIE_INFO);
+    // setTodoList(response.data);
+    console.log(MOVIE_INFO);
+  };
+  useEffect(() => {
+    fetchDataInfo();
+  }, []);
+
+  return (
+    <div className='movie-wrapper'>
+      <div className='movie-list'>
+        <h2>Movies</h2>
+        <div className='movie-container'>
+          <MovieCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostList;
